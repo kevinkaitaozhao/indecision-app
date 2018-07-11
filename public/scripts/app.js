5888033ddd9fd255cde28cb3e4e78aaae6778be4
kevinkaitaozhao <zhao.kevink@gmail.com>
2018-07-10 17:13:35 -0700

    set up workflow with babel and presets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,13 @@
-console.log('App.js is running')
+'use strict';
+
+console.log('App.js is running');
 
 // JSX - Javascript XML
-// var template = <p>This is JSX from app.js!</p>
 var template = React.createElement(
-  "p",
-  { className: "class", id: "id" },
-  "This is JSX from app.js!"
+  'h1',
+  null,
+  'Indecision App'
 );
-var appRoot = document.getElementById('app')
+var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
